b629598543de2dd19f070d43ccabc7a7ef2c5225
rxshabN <nagwanirishab@gmail.com>
2024-11-08 06:05:01 +0530

    fix

--- app/(invite)/(routes)/invite/[inviteCode]/page.tsx
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -1,3 +1,54 @@
+// import { currentProfile } from "@/lib/current-profile";
+// import { db } from "@/lib/db";
+// import { auth } from "@clerk/nextjs/server";
+// import { redirect } from "next/navigation";
+
+// interface InviteCodePageProps {
+//   params: {
+//     inviteCode: string;
+//   };
+// }
+
+// const InviteCodePage = async ({ params }: InviteCodePageProps) => {
+//   const profile = await currentProfile();
+//   if (!profile) {
+//     const authInstance = await auth();
+//     return authInstance.redirectToSignIn();
+//   }
+//   if (!params.inviteCode) {
+//     return redirect("/");
+//   }
+//   const existingServer = await db.server.findFirst({
+//     where: {
+//       inviteCode: params.inviteCode,
+//       members: {
+//         some: {
+//           profileId: profile.id,
+//         },
+//       },
+//     },
+//   });
+//   if (existingServer) {
+//     return redirect(`/servers/${existingServer.id}`);
+//   }
+//   const server = await db.server.update({
+//     where: {
+//       inviteCode: params.inviteCode,
+//     },
+//     data: {
+//       members: {
+//         create: [{ profileId: profile.id }],
+//       },
+//     },
+//   });
+//   if (server) {
+//     return redirect(`/servers/${server.id}`);
+//   }
+//   return null;
+// };
+
+// export default InviteCodePage;
+
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
@@ -9,15 +60,32 @@ interface InviteCodePageProps {
   };
 }
 
-const InviteCodePage = async ({ params }: InviteCodePageProps) => {
+const InviteCodePage = ({ params }: InviteCodePageProps) => {
+  return null; // Since the page logic is handled server-side, we don't need to return anything here.
+};
+
+export async function getServerSideProps({
+  params,
+}: {
+  params: { inviteCode: string };
+}) {
   const profile = await currentProfile();
+
   if (!profile) {
     const authInstance = await auth();
-    return authInstance.redirectToSignIn();
+    authInstance.redirectToSignIn();
+    return { props: {} }; // Will redirect from the server, so we don't need to render anything here.
   }
+
   if (!params.inviteCode) {
-    return redirect("/");
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
   }
+
   const existingServer = await db.server.findFirst({
     where: {
       inviteCode: params.inviteCode,
@@ -28,9 +96,16 @@ const InviteCodePage = async ({ params }: InviteCodePageProps) => {
       },
     },
   });
+
   if (existingServer) {
-    return redirect(`/servers/${existingServer.id}`);
+    return {
+      redirect: {
+        destination: `/servers/${existingServer.id}`,
+        permanent: false,
+      },
+    };
   }
+
   const server = await db.server.update({
     where: {
       inviteCode: params.inviteCode,
@@ -41,10 +116,17 @@ const InviteCodePage = async ({ params }: InviteCodePageProps) => {
       },
     },
   });
+
   if (server) {
-    return redirect(`/servers/${server.id}`);
+    return {
+      redirect: {
+        destination: `/servers/${server.id}`,
+        permanent: false,
+      },
+    };
   }
-  return null;
-};
+
+  return { props: {} }; // Return empty props if no redirection happened
+}
 
 export default InviteCodePage;
