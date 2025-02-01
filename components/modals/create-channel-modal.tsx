2d0619efa19c1896c2e5c96147bbe7da7e382a4b
rxshabN <nagwanirishab@gmail.com>
2025-02-02 00:05:27 +0530

    revert to tailwind v3 and shift to vercel

--- components/modals/create-channel-modal.tsx
+++ components/modals/create-channel-modal.tsx
@@ -149,7 +149,7 @@ export const CreateChannelModal = () => {
                         disabled={isLoading}
                       >
                         <FormControl>
-                          <SelectTrigger className="bg-zinc-300/50 border-0 focus:ring-0 text-black ring-offset-0 capitalize focus:ring-offset-0 outline-hidden">
+                          <SelectTrigger className="bg-zinc-300/50 border-0 focus:ring-0 text-black ring-offset-0 capitalize focus:ring-offset-0 outline-none">
                             <SelectValue placeholder="Select a channel type" />
                           </SelectTrigger>
                         </FormControl>
